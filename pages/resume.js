e200c978131de8ab427ebca2fb07c19396f9e9eb
Kelvin Jou <kelvin.jou@gmail.com>
2025-04-12 00:19:10 -0700

    resume update and under development caption removed

--- pages/resume.js
+++ pages/resume.js
@@ -15,97 +15,95 @@ import { MdOutlineArrowOutward } from "react-icons/md";
 
 
 const Resume = () => {
-  const router = useRouter();
-  const theme = useTheme();
-  const [mount, setMount] = useState(false);
+    const router = useRouter();
+    const theme = useTheme();
+    const [mount, setMount] = useState(false);
 
-  useEffect(() => {
-    setMount(true);
-    if (!showResume) {
-      router.push("/");
-    }
-  }, []);
-  return (
-    <>
-      {data.showCursor && <Cursor />}
-      <div
-        className={`container mx-auto mb-10 ${
-          data.showCursor && "cursor-none"
-        }`}
-      >
-        <Header isBlog />
-        {mount && (
-          <div className="mt-10 w-full flex flex-col items-center">
-            <Button
-              onClick={() => router.push("/images/resume_file.pdf")}>
-              <h1 className="text-2xl font-bold">View PDF Resume Version</h1>
-              <MdOutlineArrowOutward className="ml-2 mb-2 text-2xl font-bold"/>
-            </Button>
+    useEffect(() => {
+        setMount(true);
+        if (!showResume) {
+            router.push("/");
+        }
+    }, []);
+    return (
+        <>
+            {data.showCursor && <Cursor />}
             <div
-              className={`w-full mt-10 ${
-                mount && theme.theme === "dark" ? "bg-slate-800" : "bg-gray-50"
-              } max-w-4xl p-20 mob:p-5 desktop:p-20 rounded-lg shadow-sm`}
+                className={`container mx-auto mb-10 ${data.showCursor && "cursor-none"
+                    }`}
             >
-              <h1 className="text-3xl font-bold">{name}</h1>
-              <h2 className="text-xl mt-5">{resume.tagline}</h2>
-              <h2 className="w-4/5 text-xl mt-5 opacity-50">
+                <Header isBlog />
+                {mount && (
+                    <div className="mt-10 w-full flex flex-col items-center">
+                        <Button
+                            onClick={() => router.push("/images/resume_file.pdf")}>
+                            <h1 className="text-2xl font-bold">View PDF Resume Version</h1>
+                            <MdOutlineArrowOutward className="ml-2 mb-2 text-2xl font-bold" />
+                        </Button>
+                        <div
+                            className={`w-full mt-10 ${mount && theme.theme === "dark" ? "bg-slate-800" : "bg-gray-50"
+                                } max-w-4xl p-20 mob:p-5 desktop:p-20 rounded-lg shadow-sm`}
+                        >
+                            <h1 className="text-3xl font-bold">{name}</h1>
+                            <h2 className="text-xl mt-5">{resume.tagline}</h2>
+                            {/* <h2 className="w-4/5 text-xl mt-5 opacity-50">
                 {resume.description}
-              </h2>
-              <div className="mt-2">
-                <Socials />
-              </div>
-              <div className="mt-5">
-                <h1 className="text-2xl font-bold">Experience</h1>
-                <div className="flex items-center">
-                  <Button
-                    onClick={() => router.push("/images/resume_file.pdf")}>
-                    <div>Refer to PDF Resume</div>
-                    <MdOutlineArrowOutward className="ml-2 mb-2"/>
-                  </Button>
-                </div>
-              </div>
+              </h2> */}
+                            <div className="mt-2">
+                                <Socials />
+                            </div>
+                            <div className="mt-5">
+                                <h1 className="text-2xl font-bold">Experience</h1>
+                                <div className="flex items-center">
+                                    <Button
+                                        onClick={() => router.push("/images/resume_file.pdf")}>
+                                        <div>Refer to PDF Resume</div>
+                                        <MdOutlineArrowOutward className="ml-2 mb-2" />
+                                    </Button>
+                                </div>
+                            </div>
 
-              <h1 className="text-2xl font-bold">Publications</h1>
-              <div className="flex flex-col items-start">
-                <Button
-                  // onClick={() => router.push("https://arxiv.org/pdf/2308.15502")}
-                  onClick={() => {
-                    // use this method to open in a new tab
-                    window.open('https://arxiv.org/pdf/2308.15502', '_blank');
-                  }}
-                  className="flex items-center"
-                >
-                  {/* Horizontal stacking of title and icon */}
-                  <div className="flex items-center">
-                    <div
-                    className="font-bold"
-                    >On the Steganographic Capacity of Selected Learning Models</div>
-                    <MdOutlineArrowOutward className="ml-2 mb-2" />
-                  </div>
-                </Button>
+                            <h1 className="text-2xl font-bold">Publications</h1>
+                            <div className="flex flex-col items-start">
+                                <Button
+                                    // onClick={() => router.push("https://arxiv.org/pdf/2308.15502")}
+                                    onClick={() => {
+                                        // use this method to open in a new tab
+                                        window.open('https://arxiv.org/pdf/2308.15502', '_blank');
+                                    }}
+                                    className="flex items-center"
+                                >
+                                    {/* Horizontal stacking of title and icon */}
+                                    <div className="flex items-center">
+                                        <div
+                                            className="font-bold"
+                                        >On the Steganographic Capacity of Selected Learning Models</div>
+                                        <MdOutlineArrowOutward className="ml-2 mb-2" />
+                                    </div>
+                                </Button>
 
-                {/* Vertical stacking of date and authors */}
-                <div className="flex flex-col text-sm">
-                  <h3 className="opacity-75">2023</h3>
-                  <p className="mt-1 opacity-50">
-                    Rishit Agrawal, Kelvin Jou, Tanush Obili, Daksh Parikh, Samarth Prajapati, Yash Seth, Charan Sridhar, Nathan Zhang, Mark Stamp
-                  </p>
-                </div>
-              </div>
+                                {/* Vertical stacking of date and authors */}
+                                <div className="flex flex-col text-sm">
+                                    <h3 className="opacity-75">2023</h3>
+                                    <p className="mt-1 opacity-50">
+                                        Rishit Agrawal, Kelvin Jou, Tanush Obili, Daksh Parikh, Samarth Prajapati, Yash Seth, Charan Sridhar, Nathan Zhang, Mark Stamp
+                                    </p>
+                                </div>
+                            </div>
 
-              <div className="mt-5">
-                <h1 className="text-2xl font-bold">Education</h1>
-                <div className="mt-2">
-                  <h2 className="text-lg">{resume.education.universityName}</h2>
-                  <h3 className="text-sm opacity-75">
-                    {resume.education.universityDate}
-                  </h3>
-                  <p className="text-sm mt-2 opacity-50">
-                    {resume.education.universityPara}
-                  </p>
-                </div>
-              </div>
-              {/* <div className="mt-5">
+                            <div className="mt-5">
+                                <h1 className="text-2xl font-bold">Education</h1>
+                                <div className="mt-2">
+                                    <h2 className="text-lg">{resume.education.universityName}</h2>
+                                    <h3 className="text-sm opacity-75">
+                                        {resume.education.universityDate}
+                                    </h3>
+                                    <p className="text-sm mt-2 opacity-50">
+                                        {resume.education.universityPara}
+                                    </p>
+                                </div>
+                            </div>
+                            {/* <div className="mt-5">
                 <h1 className="text-2xl font-bold">Skills</h1>
                 <div className="flex mob:flex-col desktop:flex-row justify-between">
                   {resume.languages && (
@@ -148,12 +146,12 @@ const Resume = () => {
                   )}
                 </div>
               </div> */}
+                        </div>
+                    </div>
+                )}
             </div>
-          </div>
-        )}
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default Resume;
