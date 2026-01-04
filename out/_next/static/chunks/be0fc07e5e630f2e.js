924d76a1e9c87b75766363a6ec400e54340cf77e
Kelvin <kelvin.jou@gmail.com>
2026-01-03 22:29:01 -0800

    gitignore remove /out/

---
+++ out/_next/static/chunks/be0fc07e5e630f2e.js
@@ -0,0 +1,8 @@
+__turbopack_load_page_chunks__("/project", [
+  "static/chunks/0449adf275ca12e6.js",
+  "static/chunks/96bfc86adc2aa2b0.js",
+  "static/chunks/b75346e3ad2e88f2.js",
+  "static/chunks/96bdf550e92811f9.js",
+  "static/chunks/97e21576f83ecc1d.js",
+  "static/chunks/turbopack-c085053ec2df94e1.js"
+])
